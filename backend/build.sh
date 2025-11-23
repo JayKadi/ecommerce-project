78486b7e87b94d66ada9acab3f1d334f40a55d1d
JayKadi <jaykadi42@gmail.com>
2025-11-23 20:34:06 +0300

    Fix staticfiles storage

--- backend/build.sh
+++ backend/build.sh
@@ -5,5 +5,5 @@ pip install -r requirements.txt
 
 mkdir -p staticfiles
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py collectstatic --no-input --clear
+python manage.py migrate --no-input
--- backend/ecommerce/settings.py
+++ backend/ecommerce/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
